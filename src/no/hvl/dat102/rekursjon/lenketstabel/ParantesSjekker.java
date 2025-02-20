d9ab5179cc5997eea402345aff97d0416fee73ce
JohannesNA <186506@stud.hvl.no>
2025-02-20 12:39:57 +0100

    ParantesSjekker ferdig

--- src/no/hvl/dat102/rekursjon/lenketstabel/ParantesSjekker.java
+++ src/no/hvl/dat102/rekursjon/lenketstabel/ParantesSjekker.java
@@ -1,17 +1,57 @@
 package no.hvl.dat102.rekursjon.lenketstabel;
 
-public class ParantesSjekker extends TabellStabel<String> {
+import no.hvl.dat102.rekursjon.stackADT;
+
+
+public class ParantesSjekker extends TabellStabel<Character> {
 	
-	public static void sjekkParanteser(String s) {
+	public static boolean sjekkParanteser(String s) {
+		
+		stackADT<Character> st = new TabellStabel<>();
 		
 		for (int i = 0; i < s.length(); i++) {
 			
-			if (s.charAt(i).equals('{')) {
-				
+			char ch = s.charAt(i);
+			
+			if (ch == '{' || ch == '[' || ch == '(') {
+				st.push(s.charAt(i));
 			}
 			
+			if (ch == '}' || ch == ']' || ch == ')') {
+				if (st.isEmpty()) {
+					return false;
+				}
+			}
+			
+			char c;
+			
+			switch (ch) {
+			
+			case '}':
+				c = st.pop();
+				if (c == '(' || c == '[') {
+					return false;
+				}
+				break;
+			
+			case ']':
+				c = st.pop();
+				if (c == '(' || c == '{') {
+					return false;
+				}
+				break;
+				
+			case ')':
+				c = st.pop();
+				if (c == '{' || c == '[') {
+					return false;
+				}
+				break;
+			}
+			
+			
 		}
-		
+		return st.isEmpty();
 	}
 
 }
--- test/no/hvl/dat102/test/TestParantes.java
+++ test/no/hvl/dat102/test/TestParantes.java
@@ -1,46 +1,44 @@
 package no.hvl.dat102.test;
 
-import no.hvl.dat102.rekursjon.lenketstabel.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class TestParentesSjekk {
-	
+import no.hvl.dat102.rekursjon.lenketstabel.ParantesSjekker;
+
+class TestParantes {
+
 	private String s1;
 	private String s2;
 	private String s3;
 	private String s4;
 	private String s5;
-	
+
 	@BeforeEach
 	void setUp() {
-		
+
 		s1 = "{ [ ( ) ] }";
 		s2 = "{ [ ( ) }";
 		s3 = "[ ( ) ] }";
 		s4 = "{ [ ( ] ) }";
-		s5 =
-				"""
+		s5 = """
 					class HelloWorld {
 						public static void main(String[] args) {
 							System.out.println("Hello World!");
 						}
 					}
 				""";
-		
+
 	}
-	
+
 	@Test
-	void testSjekkParentes() {
+	void test() {
 		assertTrue(ParantesSjekker.sjekkParanteser(s1));
 		assertFalse(ParantesSjekker.sjekkParanteser(s2));
 		assertFalse(ParantesSjekker.sjekkParanteser(s3));
 		assertFalse(ParantesSjekker.sjekkParanteser(s4));
 		assertTrue(ParantesSjekker.sjekkParanteser(s5));
-		
 	}
-	
+
 }
