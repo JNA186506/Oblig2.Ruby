0a52d39e55faf942eda219d8babad7195bfc9848
587712 <587712@stud.hvl.no>
2025-02-20 14:16:58 +0100

    Lagt til test for quicksort med like tal

--- test/no/hvl/dat102/test/TestQuickSort.java
+++ test/no/hvl/dat102/test/TestQuickSort.java
@@ -10,12 +10,15 @@ class TestQuickSort {
 
 	private Integer[] tab1;
     private Integer[] tab2;
+    private Integer[] tab3;
 	
 	@BeforeEach
 	void setUp() {
 		
 		tab1 = new Integer[]{2,6,4,1,2,9,3};
 		tab2 = new Integer[]{1,2,2,3,4,6,9};
+		tab3 = new Integer[100000];
+		for(int i = 0; i < tab3.length; i++) tab3[i] = 1;
 		
 	}
 	
@@ -25,6 +28,13 @@ class TestQuickSort {
 		Quicksort.quickSort(tab1);
 		
 		assertArrayEquals(tab2, tab1);
+		
+	}
+	
+	@Test
+	void testSortLike() {
+		
+		Quicksort.quickSort(tab3);
 		
 	}
 
