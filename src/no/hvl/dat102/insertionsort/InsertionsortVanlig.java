4287620767ff5a8fe6aa1af4e4ff05613f4e9e75
587712 <587712@stud.hvl.no>
2025-02-20 11:26:11 +0100

    Fjerna kommentert kode

--- src/no/hvl/dat102/insertionsort/InsertionsortVanlig.java
+++ src/no/hvl/dat102/insertionsort/InsertionsortVanlig.java
@@ -7,21 +7,7 @@ public class InsertionsortVanlig {
     }
 
     public static <T extends Comparable<? super T>> void insertionSort(T[] a, int forste, int siste) {
-//
-//        T first = a[0];
-//        int idMin = 0;
-//
-//        for (int k = 0; k < siste; k++) {
-//            if (a[k].compareTo(first) < 0)  {
-//                first = a[k];
-//                idMin = k;
-//            }
-//        }
-//
-//        T temp = a[0];
-//        a[0] = first;
-//        a[idMin] = temp;
-
+    	
         for (int i = forste; i <= siste; i++) {
 
             int j = i - 1;
